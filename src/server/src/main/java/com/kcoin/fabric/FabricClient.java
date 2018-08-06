5095ac83c6b942d50b3fb8f40cc0a33e53a859e5
Junbo Wang <juniwang@microsoft.com>
2018-08-06 10:34:06 +0800

    update log level

--- src/server/src/main/java/com/kcoin/fabric/FabricClient.java
+++ src/server/src/main/java/com/kcoin/fabric/FabricClient.java
@@ -125,7 +125,7 @@ public class FabricClient {
         // where all the proposals above are consistent.
         Collection<Set<ProposalResponse>> proposalConsistencySets = SDKUtils.getProposalConsistencySets(transactionPropResp);
         if (proposalConsistencySets.size() != 1) {
-            logger.info(format("Expected only one set of consistent proposal responses but got %d",
+            logger.warn(format("Expected only one set of consistent proposal responses but got %d",
                     proposalConsistencySets.size()));
             return FabricResponse.failure().withMessage("proposals are not consistent with each other");
         }
