716a5b77d615d8e52c09bfb51d0871dc378fd781
Junbo Wang <juniwang@microsoft.com>
2018-08-25 22:51:56 +0800

    revert prod config

--- src/web/config/puma.rb
+++ src/web/config/puma.rb
@@ -1,3 +1,9 @@
-environment 'development'
+environment 'production'
 
-bind 'tcp://0.0.0.0:9292'
+threads 1,16
+workers 4
+preload_app!
+
+bind 'unix:///var/run/puma.sock'
+pidfile '/var/run/puma.pid'
+stdout_redirect '/var/log/kcoin/stdout', '/var/log/kcoin/stderr', true
