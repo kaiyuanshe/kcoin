f0279c3d7a1f0ccb5f4aa1c119887a39fbb46cc1
Junbo Wang <juniwang@microsoft.com>
2018-10-14 09:26:45 +0800

    remove email password from source code

--- src/web/lib/email_params.rb
+++
@@ -1,9 +0,0 @@
-EMAIL = {
-  form: 'kcoin@kaiyuanshe.cn',
-  address: 'smtp.exmail.qq.com',
-  port: 465,
-  helo: 'qq.com',
-  user: 'kcoin@kaiyuanshe.cn',
-  secret: 'QTE5OTI0MTQxYQ==',
-  account: 'kcoin@kaiyuanshe.cn'
-}.freeze
